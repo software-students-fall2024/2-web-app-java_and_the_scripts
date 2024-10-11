2c6eef1aa08397a2e23e5b23c2e6b3c055363e7a
jljune9li <jljune9li@gmail.com>
2024-10-11 18:31:48 -0400

    change to app

--- app/app.py
+++ app/app.py
@@ -1,42 +1,53 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pymongo
 from dotenv import load_dotenv
-app=Flask(__name__)
+load_dotenv()
 
-# Home route
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-# Route for adding a task
-@app.route('/add', methods=['GET', 'POST'])
-def add_task():
-    return render_template('data_add.html')
 
-# Route for editing a task
-@app.route('/edit', methods=['GET', 'POST'])
-def edit_task():
-    return render_template('data_edit.html')
+def create_app():
+    """
+    Create and configure the Flask application.
+    returns: app: the Flask application object
+    """
+    app=Flask(__name__)
 
-# Route for deleting a task
-@app.route('/delete', methods=['GET', 'POST'])
-def delete_task():
-    return render_template('data_delete.html')
+    # Home route
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-# Route for displaying all tasks
-@app.route('/display', methods=['GET', 'POST'])
-def display_tasks():
-    return render_template('display_all.html')
+    # Route for adding a task
+    @app.route('/add', methods=['GET', 'POST'])
+    def add_task():
+        return render_template('data_add.html')
 
-# Route for displaying pending tasks
-@app.route('/pending', methods=['GET', 'POST'])
-def pending_tasks():
-    return render_template('pending.html')
+    # Route for editing a task
+    @app.route('/edit', methods=['GET', 'POST'])
+    def edit_task():
+        return render_template('data_edit.html')
 
-# Route for searching tasks
-@app.route('/search', methods=['GET', 'POST'])
-def search_task():
-    return render_template('search.html')
+    # Route for deleting a task
+    @app.route('/delete', methods=['GET', 'POST'])
+    def delete_task():
+        return render_template('data_delete.html')
+
+    # Route for displaying all tasks
+    @app.route('/display', methods=['GET', 'POST'])
+    def display_tasks():
+        return render_template('display_all.html')
+
+    # Route for displaying pending tasks
+    @app.route('/pending', methods=['GET', 'POST'])
+    def pending_tasks():
+        return render_template('pending.html')
+
+    # Route for searching tasks
+    @app.route('/search', methods=['GET', 'POST'])
+    def search_task():
+        return render_template('search.html')
+    return app
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
